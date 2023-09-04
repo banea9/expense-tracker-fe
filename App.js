91d17772a58caaf940afec99960def003bac1a7f
branislav.aleksov <branislav.aleksov@accedia.com>
2023-09-04 16:42:10 +0300

    fix the authentication

--- App.js
+++ App.js
@@ -20,165 +20,166 @@ import WalletForm from "./components/ManageWallet/WalletForm";
 const Stack = createNativeStackNavigator();
 const Tabs = createBottomTabNavigator();
 
-export default function App() {
-  const TabsNavigation = () => {
-    return (
-      <Tabs.Navigator
-        screenOptions={() => ({
-          headerStyle: {
-            backgroundColor: GlobalStyles.colors.primary700,
-          },
-          headerTintColor: GlobalStyles.colors.white,
-          tabBarStyle: {
-            backgroundColor: GlobalStyles.colors.primary700,
-          },
-          tabBarActiveTintColor: GlobalStyles.colors.primary400,
-        })}
-      >
-        <Tabs.Screen
-          component={Expenses}
-          name="Expenses"
-          options={({ navigation }) => ({
-            tabBarLabel: "Recent",
-            title: "Expenses",
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="hourglass" color={color} size={size} />
-            ),
-            headerRight: ({ tintColor }) => (
-              <IconButton
-                icon="add"
-                color={tintColor}
-                size={24}
-                onPress={() => {
-                  navigation.navigate("ManageExpense");
-                }}
-              />
-            ),
-          })}
-        />
-        <Tabs.Screen
-          component={Wallets}
-          name="Wallets"
-          options={({ navigation }) => ({
-            tabBarLabel: "Wallets",
-            title: "Wallets",
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="briefcase" color={color} size={size} />
-            ),
-            headerRight: ({ tintColor }) => (
-              <IconButton
-                icon="add"
-                color={tintColor}
-                size={24}
-                onPress={() => {
-                  navigation.navigate("ManageWallet");
-                }}
-              />
-            ),
-          })}
-        />
-        <Tabs.Screen
-          component={AllExpenses}
-          name="AllExpenses"
-          options={{
-            tabBarLabel: "All Expenses",
-            title: "All Expenses",
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="calendar" color={color} size={size} />
-            ),
-          }}
-        />
-      </Tabs.Navigator>
-    );
-  };
-
-  const AuthStack = () => {
-    return (
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: { backgroundColor: GlobalStyles.colors.primary700 },
-          headerTintColor: GlobalStyles.colors.white,
-          contentStyle: { backgroundColor: GlobalStyles.colors.primary700 },
-        }}
-      >
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen
-          name="Signup"
-          component={SignupScreen}
-          options={{
-            title: "Register",
-          }}
-        />
-      </Stack.Navigator>
-    );
-  };
-
-  const AuthenticatedStack = () => {
-    const dispatch = useDispatch();
-
-    return (
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: GlobalStyles.colors.primary700,
-          },
-          headerTintColor: GlobalStyles.colors.white,
+const TabsNavigation = () => {
+  return (
+    <Tabs.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: GlobalStyles.colors.primary700,
+        },
+        headerTintColor: GlobalStyles.colors.white,
+        tabBarStyle: {
+          backgroundColor: GlobalStyles.colors.primary700,
+        },
+        tabBarActiveTintColor: GlobalStyles.colors.primary400,
+      }}
+    >
+      <Tabs.Screen
+        component={Expenses}
+        name="Expenses"
+        options={({ navigation }) => ({
+          tabBarLabel: "Recent",
+          title: "Expenses",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="hourglass" color={color} size={size} />
+          ),
           headerRight: ({ tintColor }) => (
             <IconButton
-              icon="exit"
+              icon="add"
               color={tintColor}
               size={24}
-              onPress={dispatch(logout())}
+              onPress={() => {
+                navigation.navigate("ManageExpense");
+              }}
             />
           ),
+        })}
+      />
+      <Tabs.Screen
+        component={Wallets}
+        name="Wallets"
+        options={({ navigation }) => ({
+          tabBarLabel: "Wallets",
+          title: "Wallets",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="briefcase" color={color} size={size} />
+          ),
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon="add"
+              color={tintColor}
+              size={24}
+              onPress={() => {
+                navigation.navigate("ManageWallet");
+              }}
+            />
+          ),
+        })}
+      />
+      <Tabs.Screen
+        component={AllExpenses}
+        name="AllExpenses"
+        options={{
+          tabBarLabel: "All Expenses",
+          title: "All Expenses",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" color={color} size={size} />
+          ),
         }}
-      >
-        <Stack.Screen
-          name="Tabs"
-          component={TabsNavigation}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="ManageExpense"
-          component={ManageExpenses}
-          options={{
-            presentation: "modal",
-          }}
-        />
-        <Stack.Screen
-          name="ManageWallet"
-          component={WalletForm}
-          options={{
-            presentation: "modal",
-          }}
-        />
-      </Stack.Navigator>
-    );
-  };
+      />
+    </Tabs.Navigator>
+  );
+};
 
-  const Root = () => {
-    const dispatch = useDispatch();
-    const authStore = useSelector((state) => state.authState);
-    const isAuthenticated = authStore.isAuthenticated;
-    useEffect(() => {
-      const fetchToken = async () => {
-        const token = await AsyncStorage.getItem("auth-token");
+const AuthStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary700 },
+        headerTintColor: GlobalStyles.colors.white,
+        contentStyle: { backgroundColor: GlobalStyles.colors.primary700 },
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen
+        name="Signup"
+        component={SignupScreen}
+        options={{
+          title: "Register",
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
 
-        if (token) {
-          dispatch(authenticate({ token }));
-        }
-      };
+const AuthenticatedStack = () => {
+  const dispatch = useDispatch();
 
-      fetchToken();
-    }, []);
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: GlobalStyles.colors.primary700,
+        },
+        headerTintColor: GlobalStyles.colors.white,
+        headerRight: ({ tintColor }) => (
+          <IconButton
+            icon="exit"
+            color={tintColor}
+            size={24}
+            onPress={() => dispatch(logout())}
+          />
+        ),
+      }}
+    >
+      <Stack.Screen
+        name="Tabs"
+        component={TabsNavigation}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ManageExpense"
+        component={ManageExpenses}
+        options={{
+          presentation: "modal",
+        }}
+      />
+      <Stack.Screen
+        name="ManageWallet"
+        component={WalletForm}
+        options={{
+          presentation: "modal",
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
 
-    return (
-      <NavigationContainer>
-        <AuthenticatedStack />
-      </NavigationContainer>
-    );
-  };
+const Root = () => {
+  const dispatch = useDispatch();
+  const authStore = useSelector((state) => state.authState);
+  const isAuthenticated = authStore.isAuthenticated;
 
+  useEffect(() => {
+    const fetchToken = async () => {
+      const token = await AsyncStorage.getItem("auth-token");
+
+      if (token) {
+        dispatch(authenticate({ token }));
+      }
+    };
+
+    fetchToken();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      {!isAuthenticated ? <AuthStack /> : <AuthenticatedStack />}
+    </NavigationContainer>
+  );
+};
+
+export default function App() {
   return (
     <>
       <StatusBar style="light" />
